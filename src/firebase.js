ff9b0c667b2c22930fdb3b3eeb9326d6906054f6
Anuj Gupta <guptaanuj0005@gmail.com>
2025-09-14 20:09:28 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,10 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: "459573470267",
   appId: "1:459573470267:android:d938091f8d1ec0a8bd281e",
   databaseURL: "https://passmate-b6bf0-default-rtdb.asia-southeast1.firebasedatabase.app"
@@ -17,5 +17,6 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+
 
 
